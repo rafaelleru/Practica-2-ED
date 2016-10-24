44edce21f6f4e3339c55784def7e4ca27c44bb3c
rafa <rafaelleru95103@gmail.com>
2016-10-24 22:34:45 +0200

    cabecera de la clase evento

---
+++ include/Evento.h
@@ -0,0 +1,29 @@
+#ifndef EVENTO
+#define EVENTO
+#include <vector>
+#include <string>
+#include <utility> //para usar pair
+
+using namespace std;
+
+class Evento{
+ private:
+  //Un evento es un par de año y acontecimientos
+  pair<int, vector<string> > evento;
+ public:
+  Evento(int date, vector<string> events);
+  Evento(Evento& e);
+  vector<string> getEvents();
+  int getDate();
+  void addEvents(vector<string> events);
+  void addSingleEvent(string event);
+
+  //operadores
+  bool operator==(Evento& e);
+  bool operator!=(Evento& e);
+  Evento& operator=(Evento& e);
+  friend ostream& operator<<(ofstream& o, Evento& e);
+  friend istream& operator>>(ifstream& i, Evento& e);
+};
+
+#endif
